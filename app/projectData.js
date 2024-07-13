beff2827101078a521561f3a36744b818de9da38
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-07-13 11:54:49 +0200

    feat: update project data

--- app/projectData.js
+++ app/projectData.js
@@ -3,7 +3,7 @@ export default [
   {
     name: 'AEC.View',
     start: '2020-02-01',
-    end: '',
+    end: '2024-08-01',
     description: 'Software to track position and status of corporate vessel fleets',
     link: '',
 
